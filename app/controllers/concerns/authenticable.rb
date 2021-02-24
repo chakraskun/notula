72f04b133aef655a8a81fbb41b5af987f6ddf77a
chakrawrasand <chakras.campur@gmail.com>
2021-02-24 13:02:20 +0700

    Adds authenticable module for managing authentication methods

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,12 @@
+module Authenticable
+    def current_team
+        return @current_team if @current_team
+
+        header = request.headers['Authorization']
+        return nil if header.nil?
+
+        decode = JsonWebToken.decode(header)
+
+        @current_team = Team.find(decoded[:team_id]) rescue ActiveRecord::RecorNotFound
+    end
+end
